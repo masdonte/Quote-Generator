a890db64960cdb24d2d596cbe98f7af97dec085d
masdonte <136394162+masdonte@users.noreply.github.com>
2025-08-25 11:07:52 +0200

    dynamic quote added

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
-let apiQuotes = [];
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+
+let apiQuotes =
+    [];
 function newQuotes() {
     const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
-    console.log(quote);
+    authorText.textContent = quote.author;
+    quoteText.textContent = quote.quote;
 }
 
 async function getQuotes() {
